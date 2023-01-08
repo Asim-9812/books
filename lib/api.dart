e8c8d42b86370dae53c2c3534df73a0a2de79bb0
Asim-9812 <asim981201@gmail.com>
2023-01-08 09:51:09 +0545

    changes

---
+++ lib/api.dart
@@ -0,0 +1,9 @@
+
+
+class Api{
+
+  static const baseUrl = 'https://api.themoviedb.org/3';
+  static const popularMovies = '$baseUrl/movie/popular';
+  static const topRatedMovies = '$baseUrl/movie/top_rated ';
+  static const upcomingMovies = '$baseUrl/movie/upcoming';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,36 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:sampleflutter/providers/counter_provider.dart';
-import 'package:sampleflutter/view/todo_hompage.dart';
-// import 'package:sampleflutter/view/Homepage.dart';
+import 'package:sampleflutter/presentation/homepage.dart';
+
+
+
+final dio = Dio();
+
+Future<void> getData () async {
+  
+  try{
+    
+    final response = await dio.get('https://api.themoviedb.org/3/movie/popular?api_key=2a0f926961d00c667e191a21c14461f8&page=1');
+    print(response.data);
+    
+  }on DioError catch(err){
+    print(err.message);
+    print(err.response);
+    
+  }
+  
+}
+
 
 
 void main(){
 
+
+  getData();
 
   runApp(ProviderScope(child: Home()));
 
@@ -29,91 +50,7 @@ class Home extends StatelessWidget {
           home: child,
         );
       },
-      child: TodoHomePage(),
+      child: Homepage(),
     );
   }
-}
-
-
-class Count extends StatelessWidget {
-
-//
-//   @override
-//   State<Count> createState() => _CountState();
-// }
-//
-// class _CountState extends State<Count> {
-//
-//   //variable declare here/function
-//   int number = 0;
-//
-//   void addNumber(){
-//     setState(() {
-//       number++;
-//     });
-//   }
-//   void minusNumber(){
-//     setState(() {
-//       number--;
-//     });
-//   }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-
-
-
-    );
-
-
-
-
-
-
-
-
-    // return Scaffold(
-    //   body: SafeArea(
-    //     child: Consumer(
-    //       builder: (context,ref,child) {
-    //         final number = ref.watch(countStateProvider);
-    //         return Column(
-    //           mainAxisAlignment: MainAxisAlignment.center,
-    //           children: [
-    //             Text('${number}', style: TextStyle(fontSize: 50.sp),),
-    //             Row(
-    //               mainAxisAlignment: MainAxisAlignment.center,
-    //               children: [
-    //                 ElevatedButton(
-    //                     onPressed: () {
-    //                      // ref.read(counterProvider).addNumber();
-    //                       ref.read(countStateProvider.notifier).state++;
-    //                     },
-    //                     child: Text('add')
-    //                 ),
-    //                 SizedBox(width: 20.w,),
-    //                 ElevatedButton(
-    //                     onPressed: () {
-    //                       // ref.read(counterProvider).minusNumber();
-    //                       ref.read(countStateProvider.notifier).state--;
-    //
-    //                     },
-    //
-    //                     child: Text('minus')
-    //                 )
-    //
-    //               ],
-    //             )
-    //
-    //           ],
-    //         );
-    //       }
-    //     ),
-    //   )
-    // );
-  }
-
-  //function
 }
